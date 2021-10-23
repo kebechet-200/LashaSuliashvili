d8d289df36b5533929960e1e8791c12360179909
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-23 14:11:49 +0400

    finish Practical_3 homework

---
+++ Day_02/Practical_3/Practical_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practical_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The pow of the entered number is {number*number}");
+        }
+    }
+}
