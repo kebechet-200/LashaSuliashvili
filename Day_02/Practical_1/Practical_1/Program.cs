8a07184ef3af7006b9a92a8893a68bd35dc30133
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-23 13:49:54 +0400

    finish Practical_1 homework

---
+++ Day_02/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer number : ");
+
+            int result = int.Parse(Console.ReadLine());
+
+            if (result % 2 == 0)
+                Console.WriteLine($"Entered number {result} is even");
+            
+            else
+                Console.WriteLine($"Entered number {result} is odd");
+        }
+    }
+}
