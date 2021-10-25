93e2f7220ef4a46362b903ddd5d17652edaf37cd
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-25 22:36:26 +0400

    finish Practice_5 homework

---
+++ Day_03/Practice_5/Practice_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number of rows of Floyd's triangle to be printed: ");
+            int zero = 0;
+            int one = 1;
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j % 2 == 1) Console.Write(zero);
+                    else Console.Write(one);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
