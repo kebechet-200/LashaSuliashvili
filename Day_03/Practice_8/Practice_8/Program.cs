e9fa8177f7e30eb01a075a17137e073d14d8b8f0
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-26 00:06:12 +0400

    finish Practice_8 homework

---
+++ Day_03/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            string output = "";
+            for (int i = inputNumber; i > 0; i/=2)
+            {
+                output += (i % 2).ToString(); // 00001
+            }
+            //Console.WriteLine(output);
+            for (int i = output.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{output[i]}");
+            }
+        }
+    }
+}
