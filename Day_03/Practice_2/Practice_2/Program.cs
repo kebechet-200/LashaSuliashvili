67497edbd06619a6326121111dd4bbf5c9f1ea22
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-25 21:54:01 +0400

    finish practice_2 homework

---
+++ Day_03/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                counter += i;
+            }
+            Console.WriteLine($"Sum from 1 to {inputNumber} is {counter}");
+        }
+    }
+}
