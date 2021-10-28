bd635ac549762aefc0a47993a9f34f088601a0a5
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-28 16:47:34 +0400

    finish Practice_4 homework

---
+++ Day_04/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size : ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arrSize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Enter number of index {i} : ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int counter = 1;
+
+            foreach (int number in array)
+            {
+                counter *= number;
+            }
+
+            Console.WriteLine($"Product of array elements is : {counter}");
+        }
+    }
+}
