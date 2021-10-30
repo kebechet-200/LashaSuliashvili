991db1007c4bd6f57609f04cc90056172f785703
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-30 17:08:06 +0400

    finish Practice_8 homework

---
+++ Day_05/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = InputNumber();
+            Calculate(result);
+        }
+
+        static double InputNumber()
+        {
+            Console.Write("Enter a positive number: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        static void Calculate(double number)
+        {
+            string printNumber = $"{number} = ";
+            for (int i = number.ToString().Length - 1; i >= 0; i--)
+            {
+                printNumber += $"{Math.Floor(number / Math.Pow(10, i))} * 10^{i} ";
+                if (i != 0) printNumber += "+ ";
+                number %= Math.Pow(10, i);
+            }
+            Console.WriteLine(printNumber);
+        }
+    }
+}
