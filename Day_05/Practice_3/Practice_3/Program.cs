307f59c7ff5d6ccef5f3349c3bf3e6a45239e604
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-10-30 14:56:46 +0400

    finish Practice_3 homework

---
+++ Day_05/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] result = reversedArray(size);
+            MinMax(result);
+        }
+
+        static int[] reversedArray(int size)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Enter integer for index {i}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] reversedArr = new int[size];
+            int j = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                reversedArr[j] = arr[i];
+                j++;
+            }
+            return reversedArr;
+        }
+
+        static void MinMax(int[] arr)
+        {
+            int min = arr[0], max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min) min = arr[i];
+                if (arr[i] > max) max = arr[i];
+            }
+
+            Console.WriteLine($"The minimum number in the array is {min}");
+            Console.WriteLine($"The maximum number in the array is {max}");
+        }
+    }
+}
