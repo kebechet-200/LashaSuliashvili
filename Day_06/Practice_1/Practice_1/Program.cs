817268e6c5f9b97a77ece301647e4ee1bd049084
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-02 19:55:52 +0400

    finish practice_1 homework

---
+++ Day_06/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Hello, world!";
+            VowelOrConsonant(inputString);
+            VowelOrConsonant(inputString, false);
+        }
+
+        static void VowelOrConsonant(string input, bool vowel = true)
+        {
+            StringBuilder vowels = new StringBuilder();
+            int counterVowels = 0;
+            StringBuilder consonants = new StringBuilder();
+            int counterConsonants = 0;
+            foreach (char letter in input)
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    counterVowels++;
+                    vowels.Append(letter + " ");
+                }
+                else
+                {
+                    if(char.IsLetter(letter))
+                    {
+                        counterConsonants++;
+                        consonants.Append(letter + "");
+                    }
+                }
+            }
+            if (vowel) Console.WriteLine($"Vowel count: {counterVowels}\nVowels: {vowels.ToString()}");
+            else Console.WriteLine($"consonants count: {counterConsonants}\nConsonants: {consonants.ToString()}");
+        }
+    }
+}
