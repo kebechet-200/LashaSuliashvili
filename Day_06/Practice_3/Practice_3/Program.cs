5534f729476fffc64b32b0a9dfb0bd56660fc758
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-02 20:16:38 +0400

    finish practice_3 homework

---
+++ Day_06/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddSpace("Goodbye World");
+        }
+
+        static void AddSpace(string input)
+        {
+            for (int i = 1; i < input.Length; i+=2)
+            {
+                input = input.Insert(i, " ");
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
