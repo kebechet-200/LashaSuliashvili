8e9dfe4964253ed61d818272fcd558941926c451
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-02 20:02:33 +0400

    finish practice_2 homework

---
+++ Day_06/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reversed("Hello");
+        }
+
+        static void Reversed(string input)
+        {
+            var charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(new string(charArray));
+        }
+    }
+}
