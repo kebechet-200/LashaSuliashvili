3466d866dfae46bf08ca3e0ad410ff19aa64f624
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-02 20:26:33 +0400

    finish practice_4 homework

---
+++ Day_06/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printNumber(WordCount("How"));
+        }
+
+        static int WordCount(string input)
+        {
+            var result = input.Split();
+            return result.Length;
+        }
+
+        static void printNumber(int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
