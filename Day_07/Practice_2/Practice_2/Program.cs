0d0fb3a501180182032772274ad7892946ec004b
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-04 00:49:40 +0400

    finish Practice_2 homework

---
+++ Day_07/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnSum(13));
+        }
+
+
+        static int ReturnSum(int number)
+        {
+            
+            if (number <= 1) return number;
+            number += ReturnSum(number - 1);
+            return number;
+        }
+
+    }
+}
