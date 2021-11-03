406b230e55b54e6bad50cbb6694a7b183841fadb
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-04 00:10:57 +0400

    finish Practice_1 homework

---
+++ Day_07/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            PrintNumberTail(input);
+            Console.WriteLine();
+            PrintNumberNoTail(input);
+        }
+
+        static void PrintNumberTail(int number)
+        {
+            if (number <= 0) return;
+            Console.Write(number + " ");
+            PrintNumberTail(number - 1);
+        }
+
+        static void PrintNumberNoTail(int number)
+        {
+            if (number <= 0) return;
+            PrintNumberNoTail(number - 1);
+            Console.Write(number + " ");
+        }
+
+        
+    }
+}
