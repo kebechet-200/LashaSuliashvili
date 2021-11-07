37b46e6994d88f45a2382929f43a59dbae963a2e
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-07 16:27:35 +0400

    finish practice_5 homework

---
+++ Day_07/Practice_5/Practice_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input text: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Is palindrome? {IsPalindrome(input)}");
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            if (input.Length < 2) return true;
+            if (input[input.Length - 1] != input[0]) return false;
+            else return IsPalindrome(input.Substring(1, input.Length - 2));
+        }
+    }
+}
