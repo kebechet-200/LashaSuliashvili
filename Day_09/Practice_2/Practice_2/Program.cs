d57384633100b57f309cd2605ee0cb92d1b13e01
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-11 19:21:04 +0400

    finish practice_2 homework

---
+++ Day_09/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            Console.Write("Enter side 1: ");
+            triangle.First = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side 2: ");
+            triangle.Second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side 3: ");
+            triangle.Third = Convert.ToInt32(Console.ReadLine());
+            if (triangle.IsValidate())
+            {
+                Console.WriteLine($"Perimeter of the triangle is: {triangle.Perimeter()}");
+                Console.WriteLine($"Area of the triangle is: {triangle.Area()}");
+            }
+            else Console.WriteLine("It is not valid triangle");
+        }
+        
+    }
+}
---
+++ Day_09/Practice_2/Practice_2/Triangle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_2
+{
+    class Triangle
+    {
+        private int a;
+        private int b;
+        private int c;
+        private bool isValidate = false;
+
+        public int First
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+
+        public int Third
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                c = value;
+
+                if ((a + b) > c &&
+                (b + c) > a &&
+                (c + a) > b)
+                {
+                    isValidate = true;
+                }
+            }
+        }
+
+        public int Perimeter()
+        {
+            return a + b + c;
+        }
+
+        public double Area()
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c)); 
+        }
+
+        public bool IsValidate()
+        {
+            if (isValidate)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        //public void IsValidate()
+        //{
+        //    if (
+        //        (a + b) > c &&
+        //        (b + c) > a &&
+        //        (c + a) > b)
+        //    {
+        //        Console.WriteLine($"Perimeter of the triangle is: {Perimeter()}");
+        //        Console.WriteLine($"Area of the triangle is: {Area()}");
+        //    }
+        //    else Console.WriteLine("It is not valid triangle"); ;
+        //}
+    }
+}
