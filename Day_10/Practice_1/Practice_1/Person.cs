6937f2c967b1eb2332f08dda8393b21e1e03625b
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-11 21:28:30 +0400

    finish practice_1 homework

---
+++ Day_10/Practice_1/Practice_1/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    class Person
+    {
+        public Person(string name, int age, string personalNumber)
+        {
+            Name = name;
+            Age = age;
+            PersonalIdNumber = personalNumber;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string PersonalIdNumber { get; set; }
+
+        public Home House { get; set; }
+    }
+
+    public class Home
+    {
+        public Home(string address, string city)
+        {
+            Address = address;
+            City = city;
+        }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Day_10/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person [3];
+            persons[0] = new Person("Lasha",27,"010012419241");
+            persons[1] = new Person("Beqa", 26, "01002104124");
+            persons[2] = new Person("Dimitri", 24, "0100120312");
+
+            Home homeForLasha = new Home("Chavchavadzis 71", "Tbilisi");
+            Home homeForBeqa = new Home("Masivi 2 mikro", "Tbilisi");
+            Home homeForDimitri = new Home("Lublianas 2/6", "Tbilisi");
+
+            persons[0].House = homeForLasha;
+            persons[1].House = homeForBeqa;
+            persons[2].House = homeForDimitri;
+        }
+    }
+}
