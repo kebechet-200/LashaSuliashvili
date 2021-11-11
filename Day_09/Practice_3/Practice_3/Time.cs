aa44262bee48defe0fded997d52e3819ac152d64
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-11 19:53:00 +0400

    finish practice_3 homework

---
+++ Day_09/Practice_3/Practice_3/Time.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_3
+{
+    class Time
+    {
+        public int Second { get; set; }
+        public int Minute { get; set; }
+        public int Hour { get; set; }
+
+        private void SubSecond()
+        {
+            if (Second != 0)
+                Second--;
+        }
+
+        private void SubMinute()
+        {
+            if (Minute != 0)
+                Minute--;
+        }
+
+        private void SubHour()
+        {
+            if(Hour != 0)
+                Hour--;
+        }
+
+        public void AddSecond()
+        {
+            if(Second < 59)
+                Second++;
+            else
+            {
+                Second = 0;
+                if(Minute < 59)
+                    Minute++;
+                else
+                {
+                    Minute = 0;
+                    if (Hour < 23)
+                        Hour++;
+                    else Hour = 0;
+                }
+            }
+
+        }
+
+        public void AddMinute()
+        {
+            if(Minute < 59)
+                Minute++;
+            else
+            {
+                Minute = 0;
+                if (Hour < 23) Hour++;
+                else Hour = 0;
+            }
+        }
+
+        public void AddHour()
+        {
+            if (Hour < 23) Hour++;
+            else Hour = 0;
+        }
+    }
+}
