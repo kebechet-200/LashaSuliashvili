46e6a19f24d61226ecfffa59297f5fb04355a080
Lasha Suliashvili <lashasuliash@gmail.com>
2021-11-14 21:03:52 +0400

    finish practice_1 homework

---
+++ Day_11/Practice_1/Practice_1/MATH.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    static class MATH
+    {
+        public static double Pow(int a, int n, out string result)
+        {
+            int number = 1;
+            if (!(n >= 0))
+            {
+                result = Statusses.PowMustBeaPositiveOrZero.ToString();
+                return 0;
+            }
+            else
+            {
+                result = Statusses.Success.ToString();
+                if (n == 0) return 1;
+                for (int i = 0; i < n; i++)
+                {
+                    number *= a;
+                }
+            }
+            return number;
+        }
+
+        public static int Min(int a, int b, out string result)
+        {
+            if (a==b)
+            {
+                result = Statusses.NumberNotBeEqual.ToString();
+                return 0;
+            }
+            else
+            {
+                result = Statusses.Success.ToString();
+                if (a > b) return b;
+                else return a;
+            }
+        }
+
+        public static int Max(int a, int b, out string result)
+        {
+            if (a == b)
+            {
+                result = Statusses.NumberNotBeEqual.ToString();
+                return 0;
+            }
+            else
+            {
+                result = Statusses.Success.ToString();
+                if (a > b) return a;
+                else return b;
+            }
+        }
+
+    }
+}
---
+++ Day_11/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // first
+            Console.WriteLine("Using MATH.Pow Function");
+            Console.WriteLine("-----------------------");
+            Console.Write("Enter a base number: ");
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter exponent: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            string result;
+            double pow = MATH.Pow(baseNumber, exponent, out result);
+
+            if (result == Statusses.Success.ToString()) Console.WriteLine(pow);
+            else Console.WriteLine(result);
+
+            // second
+            Console.WriteLine();
+            Console.WriteLine("Using MATH.Min Function");
+            Console.WriteLine("-----------------------");
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            string checkerMin;
+            int min = MATH.Min(a, b, out checkerMin);
+            if (checkerMin == Statusses.Success.ToString()) Console.WriteLine(min);
+            else Console.WriteLine(checkerMin);
+
+            //third
+            Console.WriteLine();
+            Console.WriteLine("Using MATH.Max Function");
+            Console.WriteLine("-----------------------");
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            string checkerMax;
+            int max = MATH.Max(a, b, out checkerMax);
+            if (checkerMax == Statusses.Success.ToString()) Console.WriteLine(max);
+            else Console.WriteLine(checkerMax);
+        }
+    }
+}
