6d40451a0284f36faf1ad6a3cfd764f110e644f1
Lasha Suliashvili <lashasuliash@gmail.com>
2021-12-04 14:55:32 +0100

    finish exam2

---
+++ Exam_2_Practice/Exam_2_Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exam_2_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Start Test");
+            Console.WriteLine("2.Add Test");
+            Console.Write("Choose one: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            switch (answer)
+            {
+                case 1:
+                    StartTest st = new StartTest();
+                    break;
+                case 2:
+                    
+                    Console.WriteLine("Enter the question: ");
+                    AddTest.AddQuestion(Console.ReadLine());
+                    Console.WriteLine("Enter answers : ");
+                    AddTest.AddAnswers(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+                    break;
+            }
+        }
+    }
+}
