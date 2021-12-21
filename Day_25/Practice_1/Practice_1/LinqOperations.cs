2ce5a99977d4b3cb8d037ff7caa9de42ba525d7c
Lasha Suliashvili <lashasuliash@gmail.com>
2021-12-21 19:47:14 +0100

    finish day 25 practice_1

---
+++ Day_25/Practice_1/Practice_1/LinqOperations.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice_1
+{
+    public static class LinqOperations
+    {
+        public static void EachClientOrderList(List<Customer> clist, List<Order> olist)
+        {
+            var quantity = from d in clist
+                           from o in olist
+                           where (d.CustomerID == o.CustomerID)
+                           select o.CustomerID;
+
+            foreach (var item in clist)
+            {
+                var orderItem = (from q in quantity
+                                 where q == item.CustomerID
+                                 select q).Count();
+
+                Console.WriteLine($"{item.CustomerID} : {orderItem}");
+            }
+        }
+
+        public static void EachClientSumList(List<Customer> clist, List<Order> olist)
+        {
+            var quantity = from o in olist
+                           select new { o.CustomerID, o.Price };
+
+            foreach (var item in clist)
+            {
+                var orderItem = (from q in quantity
+                                 where q.CustomerID == item.CustomerID
+                                 select q.Price).Sum();
+
+                Console.WriteLine($"{item.CustomerID} : {orderItem}");
+            }
+        }
+
+        public static void EachClientMinList(List<Customer> clist, List<Order> olist)
+        {
+            var quantity = from o in olist
+                           select new { o.CustomerID, o.Price };
+
+            foreach (var item in clist)
+            {
+                var orderItem = (from q in quantity
+                                 where q.CustomerID == item.CustomerID
+                                 select q.Price).Min();
+
+                Console.WriteLine($"{item.CustomerID} : {orderItem}");
+            }
+        }
+
+        public static void OnlyClientsMoreThanOneOrder(List<Customer> clist, List<Order> olist)
+        {
+            var quantity = from d in clist
+                           from o in olist
+                           where (d.CustomerID == o.CustomerID)
+                           select o.CustomerID;
+
+            foreach (var item in clist)
+            {
+                var orderItem = (from q in quantity
+                                 where q == item.CustomerID
+                                 select q).Count();
+
+                if (orderItem > 1)
+                    Console.WriteLine($"{item.CustomerID} : {orderItem}");
+            }
+        }
+
+        public static void OnlyClientsMoreThanAvgTen(List<Customer> clist, List<Order> olist)
+        {
+            var quantity = from d in clist
+                           from o in olist
+                           where (d.CustomerID == o.CustomerID)
+                           select new { o.CustomerID, o.Price };
+
+            foreach (var item in clist)
+            {
+                var orderItem = (from q in quantity
+                                 where q.CustomerID == item.CustomerID
+                                 select q.Price).Sum();
+
+                if (orderItem > 10)
+                    Console.WriteLine($"{item.CustomerID} : {orderItem}");
+            }
+        }
+    }
+}
---
+++ Day_25/Practice_1/Practice_1/ParseOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Practice_1
+{
+    public static class ParseOrder
+    {
+        private const string _path = "../../../Orders.txt";
+        private static List<Order> _orderList = new List<Order>();
+
+        public static List<Order> OrderParser()
+        {
+            using (StreamReader sr = new StreamReader(_path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split('|');
+                    Order c = new Order();
+                    c.OrderID = Convert.ToInt32(line[0]);
+                    c.Date = DateTime.ParseExact(line[1], "yyyyMMdd", CultureInfo.InvariantCulture);
+                    c.Product = line[2];
+                    c.Price = Convert.ToDecimal(line[3]);
+                    c.CustomerID = Convert.ToInt32(line[4]);
+                    _orderList.Add(c);
+                }
+            }
+            return _orderList;
+        }
+    }
+}
