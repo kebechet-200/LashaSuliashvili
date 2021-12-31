675d69d56c769667c16b522971d13f1492fa3a2b
Lasha Suliashvili <lashasuliash@gmail.com>
2021-12-31 18:28:17 +0100

    fix practice_1 bug (printing into file)

--- Day_28/Practice_1/Practice_1/Program.cs
+++ Day_28/Practice_1/Practice_1/Program.cs
@@ -17,58 +17,50 @@ namespace Practice_1
 
             Console.WriteLine("Press Any Key To Start, For Exit Press 'x'");
 
-            var task1 = WaitingX(cts);
-            CatchTask(po, cts);
+            // task which is depend on 'x'
+            CancelTask(cts);
 
+            // task for writing into file 
+            WriteInFile(po, cts);
         }
 
-        static void CatchTask(ParallelOptions po, CancellationTokenSource cts)
-        {
 
+        static void CancelTask(CancellationTokenSource cts)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'x')
+                {
+                    cts.Cancel();
+                }
+            });
+        }
+
+        static void WriteInFile(ParallelOptions po, CancellationTokenSource cts)
+        {
             try
             {
-                CreateAndWriteToFile2(po);
+                int from = 1;
+                int to = 11;
+                Parallel.For(from, to, po, i =>
+                {
+                    while (!po.CancellationToken.IsCancellationRequested)
+                        using (StreamWriter sw = new StreamWriter($"{i}.txt", true))
+                        {
+                            Task.Delay(i * 100);
+                            sw.WriteLine($"Task {i}");
+                        }
+
+                });
             }
             catch (OperationCanceledException ex)
             {
-                Console.WriteLine($"{ex.Message} is cancelled.");
+                Console.WriteLine($"\n{ex.Message}");
             }
             finally
             {
                 cts.Dispose();
             }
-        }
-
-        static void CreateAndWriteToFile2(ParallelOptions po)
-        {
-            int from = 1;
-            int to = 11;
-
-            Parallel.For(from, to, po, i =>
-            {
-                //string file = $"{i}.txt";
-                //File.Create(file);
-                /*StreamWriter sw = new StreamWriter(file);*///error
-                while (!po.CancellationToken.IsCancellationRequested)
-                {
-                    //StreamWriter sw = new StreamWriter(file); //error
-                    //File.AppendAllText(file,$"Task {i}"); // error
-                    Console.WriteLine($"Task {i}");
-                }
-            });
-        }
-
-        static async Task WaitingX(CancellationTokenSource cts)
-        {
-            await Task.Run(() =>
-            {
-                Console.ReadKey();
-                if (Console.ReadKey().KeyChar == 'x')
-                {
-                    cts.Cancel();
-                }
-                Console.WriteLine();
-            });
         }
     }
 }
