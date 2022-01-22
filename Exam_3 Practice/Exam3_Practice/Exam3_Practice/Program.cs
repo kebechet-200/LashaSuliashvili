17a28a72c4bafd809fa2ec4415dc49f90b0417be
Lasha Suliashvili <lashasuliash@gmail.com>
2022-01-22 14:01:00 +0100

    finish exam3

---
+++ Exam_3 Practice/Exam3_Practice/Exam3_Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam3_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> i = new List<int> { 1 };
+            i.Single();
+        }
+    }
+}
