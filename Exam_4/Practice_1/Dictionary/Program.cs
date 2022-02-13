79882fe50cc67185e5dba5660c67012e0d8c30be
Lasha Suliashvili <lashasuliash@gmail.com>
2022-02-13 11:22:10 +0100

    finish exam_4

---
+++ Exam_4/Practice_1/Dictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            var files = InputOutputOperation();
+
+            Task t1 = Task.Run(async () =>
+           {
+               await Read(files.Item1);
+           });
+
+            Task t2 = Task.Run(async () =>
+            {
+               await Read(files.Item2);
+            });
+
+            tasks.Add(t1);
+            tasks.Add(t2);
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        static (string,string) InputOutputOperation()
+        {
+            Console.Write("Enter first file name : ");
+            string firstFile = Console.ReadLine();
+            string firstPath = $"../../../{firstFile}.txt";
+
+            Console.Write("Enter second file name : ");
+            string secondFile = Console.ReadLine();
+            string secondPath = $"../../../{secondFile}.txt";
+
+            return (firstPath, secondPath);
+        }
+
+        static async Task Read(string filePath)
+        {
+            string _result; 
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    //await Task.Delay(100); // orive mushaobs
+                    _result = await sr.ReadLineAsync();
+                    Console.WriteLine(_result);
+                    await Task.Delay(100);
+                }
+            }    
+        }
+    }
+}
---
+++ Exam_4/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class Program
+    {
+        // winning numbers list
+        private static List<int> winningNumbers = new List<int>();
+        private static int _players;
+        private static Random rnd = new Random();
+        private static int winner = 0;
+
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ParallelOptions po = new ParallelOptions();
+            po.CancellationToken = cts.Token;
+
+            Start(po, cts);
+        }
+
+        static void Start(ParallelOptions po, CancellationTokenSource cts)
+        {
+            Console.Write("Please enter the number of players : ");
+            _players = Convert.ToInt32(Console.ReadLine());
+            //filling lucky numbers with input values
+            FillLuckyNumbers();
+
+            try
+            {
+                Game(po, cts);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"Player {winner} BINGOOO");
+            }
+        }
+
+        static void Game(ParallelOptions po, CancellationTokenSource cts)
+        {
+            int from = 0; int to = _players;
+
+            Parallel.For(from, to, po, i =>
+            {
+                Task.Run(() =>
+                {
+                    int count = winningNumbers.Count;
+                    while (!po.CancellationToken.IsCancellationRequested)
+                    {
+                        int number = rnd.Next(1, 100);
+                        if (winningNumbers.Contains(number))
+                        {
+                            count--;
+                        }
+                        if (count == 0)
+                        {
+                            winner = i;
+                            cts.Cancel();
+                        }
+                    }
+                });
+            });
+        }
+        static void FillLuckyNumbers()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write($"Input {i} lucky number : ");
+                winningNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+        }
+    }
+}
